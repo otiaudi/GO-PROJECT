856f9201c5ea3d086fcb4dc7107154eee684a550
odero <otiaudi9@gmail.com>
2025-05-19 00:24:23 +0300

    I have learnt arrays, pointers, and struct

---
+++ Section-1/main.go
@@ -0,0 +1,24 @@
+  package main
+
+import ("fmt"
+"strings"
+)
+
+func main(){
+/* Working with strings
+	//.......................................
+		m1:="ma name is"
+		m2 :="My age is 20"
+		fmt.Println(strings.ReplaceAll(m1,"m","h"))
+	    fmt.Println(strings.Split(m2,","))
+*/
+/* Working with numbers
+	//.......................................
+	m1:=3
+	m2:=4
+	fmt.Println(m1+m2)
+*/
+//* Working with arrays
+	//.......................................
+	 
+}
---
+++ Section-1/main2.go
@@ -0,0 +1,104 @@
+package main
+import (
+	"fmt"
+)
+/* ARRAYS
+	Declare and initialize an array
+	Access elements in an array
+	Iterate over an array
+	Modify elements in an array
+	Multidimensional arrays
+	Declare and initialize an array 
+	func todo(){
+		 
+		 arr1 :=[]int{1,2,3,4,5}
+		 for i:=0; i<len(arr1); i++{
+			 fmt.Println(arr1[i])
+		 } // Iterate over an array
+	
+
+		 arr2 :=[] string{"hello","world"}
+		 arr2=append(arr2, "that is the good news")
+		 fmt.Println(arr2)
+	}
+
+
+*/
+
+
+/* fuction to demonstrate the use of pointers
+
+	func swap(m1 *int, m2 *int) {
+	// Swap the values of m1 and m2
+	// Use a temporary variable to hold the value of m1
+	// Swap the values
+
+	 temp := *m2 // Store the value of m2 in temp
+		*m2=*m1 // Assign the value of m1 to m2
+		*m1 =temp // Assign the value of temp to m1
+	}
+*/
+
+/*
+STRUCTURE
+	Structs are used to create a custom data type that can hold multiple values of different types.
+	structs are used to group related data together.
+	it is a composite data type that can hold multiple values of different types.
+	this is called data encapsulation.
+	
+	// Declare a struct
+   type Car struct {
+		name string
+		age int
+		modelNo int
+   }
+
+   // Method of the struct
+   func(c Car) Drive(){
+	fmt.Println("The car is driving")
+   }
+	// Method of the struct
+	func(c Car) CarName() string{
+		return c.name
+	}
+*/
+func main(){
+/*	fmt.Println("Arrays")
+	todo()
+	Slices
+	Declare and initialize a slice
+	Access elements in a slice
+	Iterate over a slice
+*/
+
+/*POINTERS
+	Declare and initialize a pointer
+	---------------------------------------------
+	p1, p2 :=5,9  
+	fmt.Println(p1, p2) 
+	swap(&p1, &p2) // call the swap function with pointers
+	Swap the values of p1 and p2
+	fmt.Println(p1, p2)	
+	---------------------------------------------
+
+*/
+
+/*STRUCTS
+	Declare and initialize a struct
+
+	c:= Car{
+		name:"Toyota", 
+		age: 6,
+		modelNo: 1234}
+	Access the fields of the struct
+	fmt.Println(c.name, c.age, c.modelNo)
+	Call the method of the struct
+	c.Drive()
+  
+	Call the method of the struct
+	c.CarName()
+	fmt.Println("The car name is", c.name)
+*/
+
+
+}
