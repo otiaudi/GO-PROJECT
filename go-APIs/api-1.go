57121a5c3e551f0c5c969c21f1b169cdad349fab
odero <otiaudi9@gmail.com>
2025-06-21 12:42:52 +0300

    updated the files

--- go-APIs/api-1.go
+++ go-APIs/api-1.go
@@ -6,35 +6,48 @@ import (
 )
 
 func main() {
-	//This creates a new ServeMux, which is a request multiplexer
-	// A ServeMux matches the URL of each incoming request against a list of registered patterns
-	// and calls the handler for the pattern that most closely matches the URL.
-	
-	mux := http.NewServeMux() // Create a new ServeMux
-	// Register a handler function for the "/great" route
-	// The handler function writes "Hello world" to the response writer
-	// The handler function is called whenever a request is made to the "/great" route.
-	// The function also prints "Request Received" to the console.
-	// The response writer is used to send the response back to the client.f
-	
+	//used to create a new multiplexer 
+	//that routes incoming HTTP requests 
+	// to the correct handler functions.
+	mux := http.NewServeMux()
+
+	// Register a handler function for the "/great" path.
 	mux.HandleFunc("/great", func(w http.ResponseWriter, r *http.Request) {
+		// Log that the request was received.
 		fmt.Println("Request Received")
+		// Write a plain-text response back to the client.
 		w.Write([]byte("Hello world\n"))
 	})
-	
-	// added another route called "Hello"
-	mux.HandleFunc("/Hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Println("Second Request Recieved")
+
+	// Register another handler for the "/Hello" path.
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		
+		fmt.Println("Second Request Received")
+		// Write a response back to the client.
 		w.Write([]byte("This is my second Request"))
 	})
 
-	// Start the server on localhost:3001
-	// ListenAndServe listens on the TCP network address 
-	// and then calls Serve to handle requests on incoming connections.
-	// If the server fails to start, it will print an error message.
-
+	// Start the HTTP server on localhost port 3001.
+	// ListenAndServe takes in an address and a handler (our mux).
 	err := http.ListenAndServe("localhost:3001", mux)
 	if err != nil {
+		// If the server fails to start, log the error.
 		fmt.Println("Server error:", err)
 	}
 }
+/* This code sets up a simple HTTP server 
+   with two endpoints:"/great" and "/Hello".
+  
+   When a request is made to either 
+   endpoint, it responds with a message 
+   and logs the request.
+  
+   The server listens on localhost at port 
+   3001.To test this, you can run the code 
+   and then use a web browser or a tool 
+   like curl to access the endpoints:as
+     - For the "/great" endpoint, navigate 
+       to http://localhost:3001/great
+     - For the "/Hello" endpoint, navigate 
+	   to http://localhost:3001/Hello
+ */
